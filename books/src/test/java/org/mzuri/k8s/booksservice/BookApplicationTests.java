838aab9fed61f1a1fbb0c1c0ef9e233ce8193f82
jbeaken <jack747@gmail.com>
2020-09-08 13:12:30 +0000

    Adding test

---
+++ books/src/test/java/org/mzuri/k8s/booksservice/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mzuri.k8s.booksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
