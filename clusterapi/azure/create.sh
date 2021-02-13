70b33cb74df49bcc8469188529d76ced5c427106
jbeaken <jack747@gmail.com>
2021-02-13 12:32:16 +0000

    Removing subscription

--- clusterapi/azure/create.sh
+++ clusterapi/azure/create.sh
@@ -1,9 +1,9 @@
-export AZURE_SUBSCRIPTION_ID="b7ea67c0-56f7-4ba0-b3a9-ca2139bab515"
+export AZURE_SUBSCRIPTION_ID="****"
 
 # Create an Azure Service Principal and paste the output here
 export AZURE_TENANT_ID="c5ce3a6b-22ce-4ac0-b840-562bb8c56811"
 export AZURE_CLIENT_ID="3d188c0f-e1a7-4579-92ac-9b6079c58255"
-export AZURE_CLIENT_SECRET="9hk3WfB_M1V858zSBYaAVg_GpKA.MDflba"
+export AZURE_CLIENT_SECRET="****"
 
 # Azure cloud settings
 # To use the default public cloud, otherwise set to AzureChinaCloud|AzureGermanCloud|AzureUSGovernmentCloud
